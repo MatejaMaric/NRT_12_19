b17438e40c39d32a98ba35480fa81b719f66df4d
Mateja <mail@matejamaric.com>
2022-06-18 23:30:19 +0200

    Dodata mogucnost prikazivanja, dodavanja i brisanja oglasa...

--- node_app/index.js
+++ node_app/index.js
@@ -11,8 +11,38 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
+class Oglas {
+  static pocetniId = 0;
+
+  constructor(kategorija, datum_isteka, cena, tekst, oznaka, email) {
+    this.id = Oglas.pocetniId++;
+    this.kategorija = kategorija;
+    this.datum_isteka = datum_isteka;
+    this.cena = cena;
+    this.tekst = tekst;
+    this.oznaka = oznaka;
+    this.email = email;
+  }
+}
+
+let oglasi = [
+  new Oglas("automobili", "2022-07-14", 3000, "Toyota Corrola", "Benzin", "pera@zdera.rs"),
+  new Oglas("automobili", "2022-07-14", 2000, "Toyota Yaris", "Benzin", "mika@peric.rs"),
+];
+
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", { oglasi });
 })
+
+app.post("/", (req, res) => {
+  const { kategorija, datum_isteka, cena, tekst, oznaka, email } = req.body;
+  oglasi.push(new Oglas(kategorija, datum_isteka, cena, tekst, oznaka, email));
+  res.redirect("/");
+})
+
+app.get("/izbrisi/:id", (req, res) => {
+  oglasi = oglasi.filter(o => o.id != req.params.id);
+  res.redirect("/");
+});
 
 app.listen(port, () => console.log(`Server started on port ${port}.`));
